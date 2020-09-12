e18bd1c80aea1f68b11052fcfc44ea7f77f0f8c4
MethodZero <dilljjack@gmail.com>
2020-09-12 13:12:55 -0500

    Add form functionality.

---
+++ forms.py
@@ -0,0 +1,6 @@
+from wtforms import Form, StringField, validators
+from wtforms.fields.html5 import DecimalRangeField
+
+class SearchPageForm(Form):
+    reading_level = DecimalRangeField(default=10, render_kw={"min": 0, "max": 20, })
+    search = StringField('', [validators.InputRequired()], render_kw={"placeholder":"Search for anything..."})
