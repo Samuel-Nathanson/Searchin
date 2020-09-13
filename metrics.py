436eb1ae2f8f3d175879c3dfd1911a8652e1fe40
Samuel Eliot Nathanson <sen52@drexel.edu>
2020-09-13 03:39:51 -0400

    Asynchronous API calls

--- metrics.py
+++ metrics.py
@@ -99,3 +99,53 @@ def create_composite(readability_json):
     composite_readability = composite_readability if composite_readability >= 0 else 0
 
     return composite_readability
+
+import aiohttp
+import asyncio
+
+minell = 20
+maxell = 2000
+
+async def fetch_excerpt(session, url):
+    async with session.get(url, headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0'}, raise_for_status=True) as response:
+        res = await response.read()
+        # if res.status_code == 200:
+        soup = BeautifulSoup(res, 'html.parser')
+        # return soup
+        # # else:
+        # #     res.raise_for_status()
+        #
+        # Parse html and collect words for request
+        excerpt = ""
+        for element in soup.find_all(["p"]):
+            clean_text = element.text.strip()
+            if len(clean_text) > minell:  # Omit paragraphs that do not have useful info
+                excerpt += clean_text + (" " if clean_text[-1] == "." else ". ")
+            if len(excerpt) > maxell:  # Cutoff to avoid sending requests that are too large
+                excerpt = excerpt[:maxell]
+                break
+        return excerpt
+
+def get_readability_scores_concurrent(urls_arr):
+    readability_scores = asyncio.run(get_readability_scores(urls_arr))
+
+# Skeleton
+def getReadabilityScore_local(excerpts):
+    return 1
+
+async def get_readability_scores(urls_arr):
+    readability_scores = {}
+    excerpts = []
+    async with aiohttp.ClientSession() as session:
+        tasks = [fetch_excerpt(session, url) for url in urls_arr]
+        excerpts = await asyncio.gather(*tasks)
+    for i in range(0, len(urls_arr)):
+        readability_scores[urls_arr[i]] = getReadabilityScore_local(excerpts[i])
+
+    print(readability_scores)
+
+
+if __name__ == "__main__":
+    a = ["https://en.wikipedia.org/wiki/Hypertext"] * 9
+    get_readability_scores_concurrent(a)
