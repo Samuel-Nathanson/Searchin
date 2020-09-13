f616eb0ad0863b8b75fd1d2f31dcbac7f28e0611
Richard Sciotti <rvs20hw@gmail.com>
2020-09-12 22:49:33 -0400

    Fine tuning readability scoring

--- metrics.py
+++ metrics.py
@@ -1,7 +1,6 @@
 import requests
 import json
 from bs4 import BeautifulSoup
-import time
 
 """
 getReadability()
@@ -20,9 +19,15 @@ Parameters:
 Outputs:
     - Readability Score
 """
-def getReadability(url, minimum_element_length = 15, maximum_excerpt_length = 2500):
+def getReadability(url, minimum_element_length = 20, maximum_excerpt_length = 2500, minimum_excerpt_length = 100):
 
-    query_string = { "text" : getExcerpt(url, minimum_element_length = minimum_element_length, maximum_excerpt_length = maximum_excerpt_length)}
+    excerpt = getExcerpt(url, minimum_element_length = minimum_element_length, maximum_excerpt_length = maximum_excerpt_length)
+
+    # Page does not have enough content to pass into readability algorithms
+    if len(excerpt) < minimum_excerpt_length:
+        return None
+
+    query_string = { "text" : excerpt}
 
     # Make request to rapid api for readability metrics
     endpoint = "https://ipeirotis-readability-metrics.p.rapidapi.com/getReadabilityMetrics"
@@ -62,7 +67,7 @@ Parameters:
     - Default: 2000
     - Optional
 """
-def getExcerpt(url, minimum_element_length = 15, maximum_excerpt_length = 2000):
+def getExcerpt(url, minimum_element_length = 20, maximum_excerpt_length = 2000):
 
     # Get html from url
     try:
@@ -78,9 +83,9 @@ def getExcerpt(url, minimum_element_length = 15, maximum_excerpt_length = 2000):
     # Parse html and collect words for request
     excerpt = ""
     for element in soup.find_all(["p"]):
-        clean_text = element.text
+        clean_text = element.text.strip()
         if len(clean_text) > minimum_element_length: # Omit paragraphs that do not have useful info
-            excerpt += clean_text
+            excerpt += clean_text + (" " if clean_text[-1] == "." else ". ")
         if len(excerpt) > maximum_excerpt_length: # Cutoff to avoid sending requests that are too large
             excerpt = excerpt[:maximum_excerpt_length]
             break
@@ -89,7 +94,7 @@ def getExcerpt(url, minimum_element_length = 15, maximum_excerpt_length = 2000):
 
 def create_composite(readability_json):
 
-    composite_readability = readability_json["FLESCH_KINCAID"]
+    composite_readability = 90 - readability_json["FLESCH_READING"] # Bias scale toward harder
     composite_readability = composite_readability if composite_readability <= 100 else 100
     composite_readability = composite_readability if composite_readability >= 0 else 0
 
